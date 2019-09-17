2258a6624dc94180c84a490ad287a461a18541b4
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-09-17 15:32:34 +0530

    FEAT: initialized dotenv

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Import Routes
 const authRoutes = require('./auth/routes');
