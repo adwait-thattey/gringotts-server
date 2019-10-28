9dabf56153dec659f05bab500a78c77a9700111b
adwait-thattey <coderdude1999@gmail.com>
2019-10-28 23:42:22 +0530

    add some test case

--- gringotts-server/test.js
+++ gringotts-server/test.js
@@ -1,11 +1,31 @@
 const vapi = require('./vault/api');
-
+const axios = require('axios');
+const url = require('url');
+const config = require('./config');
+const utils = require('./vault/utils');
 user = {
-    "username":"sampleuser1"
+    "username":"sampleuser001",
+    "password": "minda"
 };
+
+/*vapi.makeVaultRequest(user,"kv/eng1/another/secret2", 'get')
+    .then( res=> {
+        console.log(res);
+    })*/
+/*
 
 vapi.createUser(user).then(()=> {
     console.log("done");
+    utils.getToken(user).then(token => {
+        console.log("token:-", token);
+        vapi.makeVaultRequest(user, "", "", "");
+    });
+
 }).catch((err)=> {
     console.log("Error", err)
+});
+*/
+
+vapi.mountNewEngine(user, "kv").then( res=> {
+    console.log(res);
 })
